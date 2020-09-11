dd425bd3fd0073597734fb15b342c2c746e72fd2
michaltruszczynski <michal.truszczynski2@gmail.com>
2020-09-11 23:52:57 +0200

    finished filter-component

--- src/App.js
+++ src/App.js
@@ -14,8 +14,38 @@ class App extends Component {
     }
   }
 
-  sortProducts = () => {
+  changeChandler = (event) => {
+    console.log('testin gevent', event.target.value)
+  }
 
+  sortProducts = (event) => {
+    const sort = event.target.value;
+    console.log(event.target.value);
+    this.setState(state => ({
+      sort: sort,
+      products: state.products.slice().sort((a, b) => (
+        sort === "lowest" ?
+          ((a.price > b.price) ? 1 : -1) :
+          sort === "highest" ?
+            ((a.price < b.price) ? 1 : -1) :
+            ((a._id < b._id) ? 1 : -1)
+      ))
+    }))
+
+  }
+
+  filterProducts = (event) => {
+    console.log(this)
+    if (event.target.value === "") {
+      this.setState({
+        products: data.products
+      })
+    } else {
+      this.setState({
+        size: event.target.value,
+        products: data.products.filter(product => product.availableSizes.indexOf(event.target.value) >= 0)
+      })
+    }
   }
 
   render() {
@@ -26,14 +56,14 @@ class App extends Component {
         </header>
         <main>
           <div className="content">
-            <div className="main">
+            <div className="main" onChange={this.changeChandler}>
               <Filter
                 count={this.state.products.length}
                 size={this.state.size}
                 sort={this.state.sort}
                 filterProducts={this.filterProducts}
                 sortProducts={this.sortProducts}
-                ></Filter>
+              ></Filter>
               <Products products={this.state.products} />
             </div>
             <div className="sidebar">
